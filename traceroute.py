81753a4c7112a9a24227a5e55a2f68078a9a9899
YassenEL <124078242+YassenEL@users.noreply.github.com>
2023-10-22 16:29:19 -0400

    Added a graph function to the traceroute
    Now after the traceroute is done, it produces a graph of what the topology would look like after all is done. I do need to see whether I want to keep both in the same file or if it would be better to separate the two.

--- traceroute.py
+++ traceroute.py
@@ -1,6 +1,8 @@
 from scapy.all import *
 import threading
 import ipaddress
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # Define the destination IP range you want to scan
 start_ip = "1.2.3.1"
@@ -9,6 +11,10 @@ end_ip = "1.2.4.2"
 # Define the maximum number of hops
 max_hops = 28
 hops = 0
+
+# Create a NetworkX graph to represent the network
+G = nx.Graph()
+
 def traceroute_ip(ip):
     global hops
     for ttl in range(1, max_hops + 1):
@@ -17,15 +23,18 @@ def traceroute_ip(ip):
         hops += 1
         if reply is None:
             # No reply
-            
             break
         elif reply.type == 0:
             # We've reached our destination
+            G.add_node(ip, ttl=ttl)
+            G.add_edge(ip, reply.src, ttl=ttl)
             print(f"Done! {ip} -> {reply.src} (TTL={ttl})")
             break
         else:
             # We're in the middle somewhere
-            print(f"We are at {hops} hops: {ip} -> {reply.src} (TTL={ttl})")
+            G.add_node(ip, ttl=ttl)
+            G.add_edge(ip, reply.src, ttl=ttl)
+            print(f"We are at {hops} hops away: {ip} -> {reply.src} (TTL={ttl})")
 
 def main():
     # Generate a list of IP addresses to scan within the specified range
@@ -47,6 +56,13 @@ def main():
     # Wait for all threads to finish
     for thread in threads:
         thread.join()
+
+    # Visualize the network graph
+    pos = nx.spring_layout(G)  # You can choose different layout algorithms
+    nx.draw(G, pos, with_labels=True)
+    labels = nx.get_edge_attributes(G, 'ttl')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    plt.show()
 
 if __name__ == "__main__":
     main()
